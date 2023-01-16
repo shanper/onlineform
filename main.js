a027b73ef66fe3de9d28e0e267c2d80df4399000
shanper <122769144+shanper@users.noreply.github.com>
2023-01-16 13:17:42 +0530

    Basic files uploaded.

---
+++ main.js
@@ -0,0 +1,86 @@
+
+
+
+//https://www.digitalocean.com/community/tutorials/submitting-ajax-forms-with-jquery
+$(document).ready(function () {
+    $("#show-modal-button").click((event)=>{
+        //clear form
+        $("#name").val("");
+        $("#email").val("");
+        $("#device").val("1");
+        $("#country").val("1");
+    });
+
+  $("form").submit(function (event) {
+    var formData = {
+      name: $("#name").val(),
+      email: $("#email").val(),
+      device: $("#device").val(),
+      country: $("#country").val()
+    };
+
+    $.ajax({
+      type: "POST",
+      url: "process.php",
+      data: formData,
+      dataType: "json",
+      encode: true,
+    }).done(function (data) {
+      console.log(data);
+
+      if (!data.success) {
+        if (data.errors.name) {
+          $("#name-group").addClass("has-error");
+          $("#name-group").append(
+            '<div class="help-block">' + data.errors.name + "</div>"
+          );
+        }
+
+        if (data.errors.email) {
+          $("#email-group").addClass("has-error");
+          $("#email-group").append(
+            '<div class="help-block">' + data.errors.email + "</div>"
+          );
+        }
+
+        if (data.errors.device) {
+          $("#superhero-group").addClass("has-error");
+          $("#superhero-group").append(
+            '<div class="help-block">' + data.errors.device + "</div>"
+          );
+        }
+
+        if (data.errors.country) {
+          $("#email-group").addClass("has-error");
+          $("#email-group").append(
+            '<div class="help-block">' + data.errors.country + "</div>"
+          );
+        }
+      } else {
+          if(data.message=="Success"){
+            $("form").html(
+                `<div class="alert alert-success">
+                    <h3>
+                        Thank You For Joining Us.
+                    </h3>
+                    <h4>
+                        We will contact you soon.
+                    </h4>
+                </div>
+                `
+            );
+            $("#show-modal-button").hide();
+            setTimeout(function() {
+                $("#close-modal").click();
+            }, 3000);
+            
+          }
+
+      }
+    });
+
+    event.preventDefault();
+  });
+});
+
+
---
+++ process.php
@@ -0,0 +1,73 @@
+<?php
+
+include_once("../posts/connection.php");
+$errors = [];
+$data = [];
+
+if (empty($_POST['name'])) {
+    $errors['name'] = 'Name is required.';
+}
+
+if (empty($_POST['email'])) {
+    $errors['email'] = 'Email is required.';
+}
+
+if (empty($_POST['country'])) {
+    $errors['country'] = 'Country is required.';
+}
+
+if (empty($_POST['device'])) {
+    $errors['device'] = 'Device is required.';
+}
+
+if (!empty($errors)) {
+    $data['success'] = false;
+    $data['errors'] = $errors;
+} else {
+    // $conn = mysqli_connect("localhost", "guest", "guest", "jutu");
+        
+    // // Check connection
+    // if($conn === false){
+    //     die("ERROR: Could not connect. "
+    //         . mysqli_connect_error());
+    // }
+    $conn = get_connection();
+    
+    // Taking all 5 values from the form data(input)
+    $name =  $_POST['name'];    
+    $email = $_POST['email'];
+    $country = $_POST['country'];
+    $device = $_POST['device'];
+        
+    // Performing insert query execution
+    // here our table name is college
+    $sql = "INSERT INTO contacts(`name`, `email`, `device`)  
+    VALUES ('$name',
+        '$email','$device')";
+    if(mysqli_query($conn, $sql)){
+        // echo "<h3>data stored in a database successfully."
+        //     . " Please browse your localhost php my admin"
+        //     . " to view the updated data</h3>";
+
+    } else{
+        // echo "ERROR: Hush! Sorry $sql. "
+        //     . mysqli_error($conn);
+            $errors['data']="Server Error. Pleas try again later.";
+    }
+    
+        
+    // Close connection
+    mysqli_close($conn);
+
+    if (!empty($errors)) {
+        $data['success'] = false;
+        $data['errors'] = $errors;
+    } else {
+        $data['success'] = true;
+        $data['message'] = 'Success';
+    }
+
+
+}
+
+echo json_encode($data);
